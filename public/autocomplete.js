326603abf44cb8f245cbac331727c0082e41d468
Deepak <deepakdas538@gmail.com>
2024-02-11 20:32:38 +0530

    added test retrival

--- public/autocomplete.js
+++ public/autocomplete.js
@@ -2,119 +2,119 @@
 
 $(document).ready(function() {
     //Define your list of diseases
-    var diseases = [
-        // A
-        "Acute Flaccid Myelitis (AFM)",
-        "Adenovirus",
-        "Anthrax",
-        "Avian Influenza",
-        // B
-        "Botulism",
-        "Blue-Green Algae",
-        "Brucellosis",
-        // C
-        "Campylobacteriosis",
-        "Chagas Disease (American trypanosomiasis)",
-        "Chickenpox",
-        "Chikungunya",
-        "Cholera",
-        "Congenital Rubella Syndrome",
-        "COVID-19 (Coronavirus Disease 2019)",
-        "Creutzfeldt-Jakob Disease",
-        "Cryptosporidiosis",
-        "Cyclosporiasis",
-        "Cytomegalovirus",
-        // D
-        "Dengue Fever",
-        "Diphtheria",
-        // E
-        "E. coli",
-        "Ebola Virus Disease",
-        "Ehrlichiosis",
-        "Enteroviruses",
-        // F
-        "Fifth Disease",
-        // G
-        "Giardiasis",
-        // H
-        "Haemophilus Influenzae Invasive Disease",
-        "Hand, Foot, and Mouth Disease",
-        "Hantavirus",
-        "Head Lice",
-        "Heartland Virus",
-        "Hemolytic Uremic Syndrome",
-        "Hepatitis A",
-        // I
-        "Influenza",
-        // L
-        "Legionellosis",
-        "Leprosy (Hansen's Disease)",
-        "Leptospirosis",
-        "Listeriosis",
-        "Lyme Disease",
-        // M
-        "Malaria",
-        "Marburg Virus Disease",
-        "Measles (Rubeola)",
-        "Meningitis",
-        "Meningococcal Disease",
-        "Molluscum Contagiosum",
-        "Mpox",
-        "Mononucleosis",
-        "Mumps",
-        // N
-        "Norovirus",
-        // P
-        "Pertussis",
-        "Plague",
-        "Polio (polio myelitis)",
-        "Primary Amebic Meningoencephalitis (PAM)",
-        "Psittacosis",
-        // Q
-        "Q Fever",
-        // R
-        "Rabies",
-        "Reye Syndrome",
-        "Ringworm",
-        "Rocky Mountain Spotted Fever",
-        "Rotavirus",
-        "Rubella (German Measles)",
-        // S
-        "Salmonellosis",
-        "Scabies",
-        "Shiga toxin-producing E. coli (STEC)",
-        "Shigellosis",
-        "Shingles",
-        "Smallpox",
-        "Southern Tick-Associated Rash Illness (STARI)",
-        "Streptococcus, group A, invasive disease",
-        "Streptococcus pneumoniae, invasive disease",
-        "Tetanus",
-        "Toxoplasmosis",
-        "Trichinellosis",
-        "Tuberculosis",
-        "Tularemia",
-        "Typhoid Fever",
-        "Vibrio species",
-        "West Nile Virus",
-        "Yellow Fever",
-        "Zika Virus"
+    // var diseases = [
+    //     // A
+    //     "Acute Flaccid Myelitis (AFM)",
+    //     "Adenovirus",
+    //     "Anthrax",
+    //     "Avian Influenza",
+    //     // B
+    //     "Botulism",
+    //     "Blue-Green Algae",
+    //     "Brucellosis",
+    //     // C
+    //     "Campylobacteriosis",
+    //     "Chagas Disease (American trypanosomiasis)",
+    //     "Chickenpox",
+    //     "Chikungunya",
+    //     "Cholera",
+    //     "Congenital Rubella Syndrome",
+    //     "COVID-19 (Coronavirus Disease 2019)",
+    //     "Creutzfeldt-Jakob Disease",
+    //     "Cryptosporidiosis",
+    //     "Cyclosporiasis",
+    //     "Cytomegalovirus",
+    //     // D
+    //     "Dengue Fever",
+    //     "Diphtheria",
+    //     // E
+    //     "E. coli",
+    //     "Ebola Virus Disease",
+    //     "Ehrlichiosis",
+    //     "Enteroviruses",
+    //     // F
+    //     "Fifth Disease",
+    //     // G
+    //     "Giardiasis",
+    //     // H
+    //     "Haemophilus Influenzae Invasive Disease",
+    //     "Hand, Foot, and Mouth Disease",
+    //     "Hantavirus",
+    //     "Head Lice",
+    //     "Heartland Virus",
+    //     "Hemolytic Uremic Syndrome",
+    //     "Hepatitis A",
+    //     // I
+    //     "Influenza",
+    //     // L
+    //     "Legionellosis",
+    //     "Leprosy (Hansen's Disease)",
+    //     "Leptospirosis",
+    //     "Listeriosis",
+    //     "Lyme Disease",
+    //     // M
+    //     "Malaria",
+    //     "Marburg Virus Disease",
+    //     "Measles (Rubeola)",
+    //     "Meningitis",
+    //     "Meningococcal Disease",
+    //     "Molluscum Contagiosum",
+    //     "Mpox",
+    //     "Mononucleosis",
+    //     "Mumps",
+    //     // N
+    //     "Norovirus",
+    //     // P
+    //     "Pertussis",
+    //     "Plague",
+    //     "Polio (polio myelitis)",
+    //     "Primary Amebic Meningoencephalitis (PAM)",
+    //     "Psittacosis",
+    //     // Q
+    //     "Q Fever",
+    //     // R
+    //     "Rabies",
+    //     "Reye Syndrome",
+    //     "Ringworm",
+    //     "Rocky Mountain Spotted Fever",
+    //     "Rotavirus",
+    //     "Rubella (German Measles)",
+    //     // S
+    //     "Salmonellosis",
+    //     "Scabies",
+    //     "Shiga toxin-producing E. coli (STEC)",
+    //     "Shigellosis",
+    //     "Shingles",
+    //     "Smallpox",
+    //     "Southern Tick-Associated Rash Illness (STARI)",
+    //     "Streptococcus, group A, invasive disease",
+    //     "Streptococcus pneumoniae, invasive disease",
+    //     "Tetanus",
+    //     "Toxoplasmosis",
+    //     "Trichinellosis",
+    //     "Tuberculosis",
+    //     "Tularemia",
+    //     "Typhoid Fever",
+    //     "Vibrio species",
+    //     "West Nile Virus",
+    //     "Yellow Fever",
+    //     "Zika Virus"
       
-    ];
+    // ];
 
 
-    // const diseases = [];
-    // axios.get('http://localhost:3000/illness')
-    //     .then(function (response) {
-    //       // Handle success: Log the response data to the console
-    //       response.data.map((data) => {
-    //         diseases.push(data.condition);
-    //       })
-    //     })
-    //     .catch(function (error) {
-    //       // Handle error: Log the error to the console
-    //       console.error(error);
-    //     });
+    const diseases = [];
+    axios.get('http://localhost:3000/illness')
+        .then(function (response) {
+          // Handle success: Log the response data to the console
+          response.data.map((data) => {
+            diseases.push(data.condition);
+          })
+        })
+        .catch(function (error) {
+          // Handle error: Log the error to the console
+          console.error(error);
+        });
     // Initialize Autocomplete
     $("#medicalHistory").autocomplete({
       source: diseases,
